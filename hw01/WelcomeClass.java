8f252acbba137e746f1e88f1bc54d3917b858e9f
kdm217 <kdm217@lehigh.edu>
2014-08-31 20:47:04 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////
+//Keri McGlothlin
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //print this statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-K--D--M--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("My name is Keri McGlothlin and I am a ballet dancer and will be helping choreograph the musical this year.");
+        
+    }
+}
