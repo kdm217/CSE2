7481c3bb269de494aa85cead122f8f85aa6fb2f7
kdm217 <kdm217@lehigh.edu>
2014-09-19 02:17:23 +0000

    updated BigMacAgain

---
+++ lab04/BigMacAgain.java
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////
+//Keri McGlothlin
+//CSE 002-110 
+//September 10th 2014
+//Four Digit Java Program
+//This program takes the user input of how many big macs they want and whether
+// or not they want fries. It should report a statement to reenter if entered incorrectly.
+
+import java.util.Scanner; //import scanner
+
+public class BigMacAgain{ //main method
+
+   	public static void main(String[] args) {
+   	    
+   	    Scanner myScanner; //declare instance of Scanner object
+   	    myScanner = new Scanner( System.in ); //create instance of scanner
+   	    
+       	    //prompt the user for amounnt of BigMacs
+       	    System.out.print("Enter the number of Big Macs: "); //prompt user
+       	    
+       	    if(myScanner.hasNextInt()){ //if the answer is an int
+       	        int nBigMacs = myScanner.nextInt(); //set nBigMacs to the int
+       	        
+       	        if (nBigMacs > 0){ //if the number is greater than 0
+                System.out.println("You ordered "+nBigMacs+" Big Macs for a cost of"
+                +nBigMacs+"x2.22 ="+(2.22*nBigMacs)); //print the cost of Big Macs
+                    
+       	            }else{ //the number is not greater than 0
+                    System.out.println("You did not enter a number greater than 0");
+                    return;    //leaves the program
+                    }
+           	        
+           	        System.out.print("Do you want an order of fries (Y/y/N/n)? ");
+                    String answer = myScanner.next(); //take the letter answer 
+                    
+                    if (answer.equals("Y")){ //if the user inputted Y
+                        //print out the cost of fries
+                        System.out.println("You ordered fries at a cost of $2.15");
+                        
+                        }else if (answer.equals("y")){ //if the user inputted y
+                            //print out the cost of fries
+                            System.out.println("You ordered fries at a cost of $2.15");
+                            
+                        }else if (answer.equals("N")){ //if the user inputted N
+                            return;
+                            
+                        }else if (answer.equals("n")){ //if the user inputted n
+                            return;
+                    
+                        }else{ //the user put in something not a Y,y,N,or n
+                            System.out.println("You did not enter a y,Y,n,or N");
+                            return;    //leaves the program
+                        }
+                        
+                    System.out.println("The total cost of the meal is "+(2.15+2.22
+                    *nBigMacs)); //print the total cost of the meal
+            
+       	    
+       	        }else{ //the nBigMac was not an int
+                    System.out.println("You did not enter an int"); //tell user why
+                    return;    //leaves the program
+                    }
+            
+            
+    }  //end of main method
+    
+} //end of class
